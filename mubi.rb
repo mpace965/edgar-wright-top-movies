e61ecaa55cdb6b1a19660dac27fd6abc00e26e2a
Matt Pace <pace4@purdue.edu>
2016-08-07 12:28:10 -0400

    Move VCR config into separate file

--- mubi.rb
+++ mubi.rb
@@ -3,10 +3,7 @@ require 'nokogiri'
 require 'json'
 require 'vcr'
 
-VCR.configure do |c|
-  c.cassette_library_dir = 'vcr_cassettes'
-  c.hook_into :webmock
-end
+require './vcr_config'
 
 def get_movie_list
   VCR.use_cassette('mubi_list_films') do
