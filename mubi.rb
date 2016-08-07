88c2bb2b411dd23d23445477d1f18e5c061737ee
Matt Pace <pace4@purdue.edu>
2016-08-07 11:33:18 -0400

    Correct spacing

--- mubi.rb
+++ mubi.rb
@@ -3,12 +3,12 @@ require 'nokogiri'
 require 'json'
 
 def get_movie_list
- json = (1..21).to_a.map do |page|
+  json = (1..21).to_a.map do |page|
    puts page
    JSON.parse RestClient.get 'https://mubi.com/services/api/lists/108835/list_films', params: { page: page }
- end
+  end
 
- json.flatten.uniq
+  json.flatten.uniq
 end
 
 def get_runtime_for_movie(movie_id)
